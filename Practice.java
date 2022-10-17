4ed158918a6f519b209dcc6115ab10d2c1264c95
SWAPNIL <73814033+swapnil-23@users.noreply.github.com>
2022-10-17 14:32:58 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+    }
+
+    for(int i = 0; i<4; i++)
+    {
+        System.out.println(int);
+        int a = 10;
+    }
+}
